4d403655936aa4a32bad9253e813c1bd8cb9d622
vinnylop <89953168+vinnylop@users.noreply.github.com>
2024-03-14 12:49:46 -0300

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,14 @@
+def verifica_fibonacci(numero):
+    a, b = 0, 1
+    while b < numero:
+        a, b = b, a + b
+    if b == numero:
+        return True
+    else:
+        return False
+
+numero_informado = int(input("Informe um número: "))
+if verifica_fibonacci(numero_informado):
+    print(f"O número {numero_informado} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {numero_informado} não pertence à sequência de Fibonacci.")
